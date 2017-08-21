65bc87a53f94fa06b2696488acda51de1ed2ab4f
rajaneeshkethavarapu <rajaneeshkethavarapu85@gmail.com>
2017-08-21 13:35:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleone',function(req,res){
+ res.send('article-one called');   
+});
+
+app.get('/articletwo',function(req,res){
+ res.send('article-two called');   
+});
+
+app.get('/articlethree',function(req,res){
+ res.send('article-three called');   
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
